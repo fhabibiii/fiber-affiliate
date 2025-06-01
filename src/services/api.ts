603d5e5058596d6cf9bd3fd199c1e4b99956c114
fhabibiii <faqihuddinh217@gmail.com>
2025-06-01 16:51:37 +0700

    Update Base URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 
 import { LoginCredentials, AuthResponse, User } from '@/types/auth';
 
-const API_BASE_URL = 'https://315b-2404-c0-3740-00-a64c-1454.ngrok-free.app/api/v1';
+const API_BASE_URL = 'http://localhost:3001/api/v1';
 
 export interface ApiResponse<T> {
   success: boolean;
